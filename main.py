34009799e3e9bcdb06d9f224461a1cd76b37edc5
Cigashamwa byamungu <ebyamungu28@gmail.com>
2020-07-14 17:04:15 +0000

    5 more print statements.

---
+++ main.py
@@ -0,0 +1,11 @@
+a = "Cigashawmwa Byamungu"
+b = "One of my favorite artist is Gunna and one of my favorite album is Drip or Drown 2 by Gunna and one of my favorite song of his is who you foolin"
+c = "I would like to take a trip to Bora Bora"
+d = "I like Track and Field"
+e = "7/14/2020"
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
